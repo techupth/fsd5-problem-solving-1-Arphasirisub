4f9886129909b6658a6ca77d649d69bf89434d21
Am <arphasirisubb@gmail.com>
2023-12-20 17:31:43 +0700

    correctt ans ex2

--- ex-2.js
+++ ex-2.js
@@ -2,8 +2,9 @@
 
 const isPalindrome = function (s) {
   //Start Coding here
-  const word = s;
-  const reverseWord = s.split("").reverse().join("");
+  const word = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
+  console.log(word)
+  const reverseWord = word.split("").reverse().join("");
   if (word === reverseWord) {
     return true;
   } else {
